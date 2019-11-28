3e1ce86ee9978437dd994f6fb9856e218c51584a
Jerome <thejerome@mail.ru>
2019-11-29 00:30:55 +0300

    task-2 tests

---
+++ src/test/java/com/efimchik/ifmo/web/mvc/MvcTestsError.java
@@ -0,0 +1,22 @@
+package com.efimchik.ifmo.web.mvc;
+
+public class MvcTestsError extends Error {
+    public MvcTestsError() {
+    }
+
+    public MvcTestsError(final String message) {
+        super(message);
+    }
+
+    public MvcTestsError(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public MvcTestsError(final Throwable cause) {
+        super(cause);
+    }
+
+    public MvcTestsError(final String message, final Throwable cause, final boolean enableSuppression, final boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
