9cb3b59a9b53ac57f5dbf95792ac9f76d3416a76
sergeypesegov <54762148+sergeypesegov@users.noreply.github.com>
2019-12-23 22:46:46 +0300

    task-1 done

--- src/main/java/com/efimchik/ifmo/web/mvc/CalcController.java
+++ src/main/java/com/efimchik/ifmo/web/mvc/CalcController.java
@@ -1,7 +1,5 @@
 package com.efimchik.ifmo.web.mvc;
 
-package com.efimchik.ifmo.web.mvc;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
