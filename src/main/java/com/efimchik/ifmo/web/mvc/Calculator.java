4e9f144bfa2a5f79ea411d8fcac40de88950288a
Gena Ziganshin <gena.ziganshin@yandex.ru>
2019-12-25 22:45:05 +0300

    It works

---
+++ src/main/java/com/efimchik/ifmo/web/mvc/Calculator.java
@@ -0,0 +1,108 @@
+package com.efimchik.ifmo.web.mvc;
+
+import javafx.util.Pair;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Calculator {
+    public String calculate(HashMap<String, String> p) {
+        try {
+        String equation = toNormForm(p);
+        while (equation.indexOf('(') != -1) {
+            int leftBorder = equation.lastIndexOf('(');
+            int rightBorder = equation.indexOf(')', leftBorder);
+            if (rightBorder < equation.length() - 1)
+                equation = equation.substring(0, leftBorder) +
+                        doOperations(equation.substring(leftBorder, rightBorder + 1)) +
+                        equation.substring(rightBorder + 1);
+            else
+                equation = equation.substring(0, leftBorder) +
+                        doOperations(equation.substring(leftBorder, rightBorder + 1));
+        }
+        return equation;
+        }catch (Exception e){
+            return "problems in calculate";
+        }
+    }
+
+
+    private String toNormForm(Map<String, String> p) {
+        HashMap<String, String> variables = new HashMap<>();
+        String equation = "";
+        for (String k : p.keySet()) {
+            if ("equation".equals(k)) {
+                equation = p.get(k);
+            } else if (variables.containsKey(p.get(k))) {
+                variables.put(k, variables.get(p.get(k)));
+            } else {
+                variables.put(k, p.get(k));
+            }
+        }
+        equation = equation.replace(" ", "");
+        equation = equation.replace("-", "+-");
+        StringJoiner sJoiner = new StringJoiner("");
+        String[] elements = equation.split("");
+        for (int i = 0; i < elements.length; i++) {
+            if (variables.containsKey(elements[i])) {
+                elements[i] = variables.get(elements[i]);
+            }
+            sJoiner.add(elements[i]);
+        }
+        return '(' + sJoiner.toString() + ')';
+    }
+
+    private String doOperations(String partOfEquation) {
+        String result = partOfEquation;
+        int i = 0;
+        while (i != -1) {
+            i = findNextOperation(result);
+            if (i != -1) {
+                Pair<Integer, Integer> p = findBorderOfOperation(result, i);
+                int res = doThisOperation(result.substring(p.getKey() + 1, p.getValue()), result.charAt(i));
+                result = result.substring(0, p.getKey() + 1) + res + result.substring(p.getValue());
+            }
+        }
+        return result.substring(1, result.length() - 1);
+    }
+
+    private int findNextOperation(String partOfEquation) {
+        int firstMul = partOfEquation.indexOf('*');
+        int firstDiv = partOfEquation.indexOf('/');
+        int i = -1;
+        if (firstMul != -1) {
+            i = firstMul;
+            if (firstDiv != -1) i = (firstMul < firstDiv) ? firstMul : firstDiv;
+        } else {
+            if (firstDiv != -1) i = firstDiv;
+        }
+        if (i == -1) i = partOfEquation.indexOf('+');
+        return i;
+    }
+
+    private Pair<Integer, Integer> findBorderOfOperation(String partOfEquation, int i) {
+        int l = i - 1;
+        while ( partOfEquation.charAt(l) != '(' &&
+                partOfEquation.charAt(l) != '+' &&
+                partOfEquation.charAt(l) != '*' &&
+                partOfEquation.charAt(l) != '/') l--;
+        int r = i + 1;
+        while ( partOfEquation.charAt(r) != ')' &&
+                partOfEquation.charAt(r) != '+' &&
+                partOfEquation.charAt(r) != '*' &&
+                partOfEquation.charAt(r) != '/') r++;
+        return new Pair<>(l, r);
+    }
+
+    private int doThisOperation(String partOfEquation, char c) {
+        int i = partOfEquation.indexOf(c);
+        int leftValue = Integer.parseInt(partOfEquation.substring(0, i));
+        int rightValue = Integer.parseInt(partOfEquation.substring(i + 1));
+        int result = leftValue;
+        if (partOfEquation.charAt(i) == '*') result *= rightValue;
+        else if (partOfEquation.charAt(i) == '/') result /= rightValue;
+        else if (partOfEquation.charAt(i) == '+') result += rightValue;
+        return result;
+    }
+}
--- src/main/java/com/efimchik/ifmo/web/mvc/MvcApplication.java
+++ src/main/java/com/efimchik/ifmo/web/mvc/MvcApplication.java
@@ -2,8 +2,17 @@ package com.efimchik.ifmo.web.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.Enumeration;
+import java.util.HashMap;
 
 
+@Controller
 @SpringBootApplication
 public class MvcApplication {
 
@@ -11,6 +20,56 @@ public class MvcApplication {
         SpringApplication.run(MvcApplication.class, args);
     }
 
+    @PutMapping(value = "/calc/{variableName}")
+    public ResponseEntity<String> putVariable(@PathVariable String variableName, @RequestBody String value, HttpSession session){
+        String message = new Checker().checkIsItOK(variableName, value);
+        if ("OK".equals(message)){
+            if (session.getAttribute(variableName) != null){
+                session.setAttribute(variableName, value);
+                return new ResponseEntity<>("you've change something", HttpStatus.valueOf(200));
+            }else{
+                session.setAttribute(variableName, value);
+                return new ResponseEntity<>("you've put something new", HttpStatus.valueOf(201));
+            }
+        }else{
+            if ("Bad variable value".equals(message)){
+                return new ResponseEntity<>(message, HttpStatus.valueOf(403));
+            }else{
+                return new ResponseEntity<>(message, HttpStatus.valueOf(400));
+            }
+        }
+    }
+
+    @GetMapping(value = "/calc/result")
+    public ResponseEntity<String> getResult(HttpSession session){
+        HashMap<String, String> p = new HashMap<>();
+        Enumeration<String> namesAr = session.getAttributeNames();
+        while (namesAr.hasMoreElements()){
+            String nameAr = namesAr.nextElement();
+            p.put(nameAr, (String)session.getAttribute(nameAr));
+        }
+        String ans = new Calculator().calculate(p);
+        if ("problems in calculate".equals(ans)){
+            return new ResponseEntity<>(ans,
+                    HttpStatus.valueOf(409));
+        }else{
+            return new ResponseEntity<>(ans, HttpStatus.valueOf(200));
+        }
+    }
+
+    @DeleteMapping(value = "/calc/{variableName}")
+    public ResponseEntity<String> deleteSomething(@PathVariable String variableName, HttpSession session){
+        if (session != null){
+            if (session.getAttribute(variableName) != null){
+                session.removeAttribute(variableName);
+                return new ResponseEntity<>("you've deleted something", HttpStatus.valueOf(204));
+            }else{
+                return new ResponseEntity<>("You're doing something bad", HttpStatus.valueOf(400));
+            }
+        }else{
+            return new ResponseEntity<>("You're doing something bad", HttpStatus.valueOf(400));
+        }
+    }
 }
 
 
