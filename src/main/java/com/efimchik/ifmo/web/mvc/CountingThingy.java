236edc6de28541f61126dd9d567bc22b3a76b494
TheTh1rt33nth <38680679+TheTh1rt33nth@users.noreply.github.com>
2019-12-26 21:54:59 +0300

    Update CountingThingy.java

--- src/main/java/com/efimchik/ifmo/web/mvc/CountingThingy.java
+++ src/main/java/com/efimchik/ifmo/web/mvc/CountingThingy.java
@@ -8,11 +8,11 @@ public class CountingThingy
     {
         if ((s.indexOf('+') == 1 || s.indexOf('-') == 1 || s.indexOf('*') == 1 || s.indexOf('/') == 1))
         {
-            return true
+            return true;
         }
         else
         {
-            return false
+            return false;
         }
     }
     private static int calculate(ArrayDeque<String> q) {
