041f20affae97d46358ce9f3247b1bd5567db102
TheTh1rt33nth <38680679+TheTh1rt33nth@users.noreply.github.com>
2019-12-26 21:55:24 +0300

    Update EqController.java

--- src/main/java/com/efimchik/ifmo/web/mvc/EqController.java
+++ src/main/java/com/efimchik/ifmo/web/mvc/EqController.java
@@ -14,11 +14,11 @@ public class EqController {
     {
         if ((s.indexOf('+') == 1 || s.indexOf('-') == 1 || s.indexOf('*') == 1 || s.indexOf('/') == 1))
         {
-            return true
+            return true;
         }
         else
         {
-            return false
+            return false;
         }
     }
     @PutMapping("/calc/equation")
