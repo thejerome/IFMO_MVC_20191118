7495ec1b5a922ec8677758613e0bad6f143efd49
ekaterinadidenko20 <ekaterinadidenko20@gmail.com>
2019-12-27 23:53:45 +0300

    --

---
+++ src/main/java/com/efimchik/ifmo/web/mvc/MainController.java
@@ -0,0 +1,10 @@
+package com.efimchik.ifmo.web.mvc;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+    ConnectionSource connectionSource = ConnectionSource.instance();
+
+
+}
