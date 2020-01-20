83d01ef9c79aa3c941e62de956f76955d6f9b72c
KostMil <55847173+KostMil@users.noreply.github.com>
2020-01-21 00:05:00 +0300

    pls

---
+++ src/main/java/com/efimchik/ifmo/web/mvc/controllers/EquationController.java
@@ -0,0 +1,53 @@
+package com.efimchik.ifmo.web.mvc.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.regex.Pattern;
+
+@RestController
+public class EquationController {
+    @PutMapping("/calc/equation")
+    public ResponseEntity<String> putEq(HttpSession httpSession, @RequestBody String body){
+        if (!goodInput(body)) {
+            return new ResponseEntity<>(HttpStatus.valueOf(400));
+        } else {
+            if (httpSession.getAttribute("equation") == null) {
+                httpSession.setAttribute("equation", body);
+                return new ResponseEntity<>(HttpStatus.valueOf(201));
+            } else {
+                httpSession.setAttribute("equation", body);
+                return new ResponseEntity<>( HttpStatus.valueOf(200));
+            }
+        }
+    }
+
+    @DeleteMapping("/calc/equation")
+    public ResponseEntity delEq(HttpSession httpSession){
+        httpSession.setAttribute("equation", null);
+        return new ResponseEntity<>(HttpStatus.valueOf(204));
+    }
+
+    private boolean goodInput(String equation) {
+        int indexZero = 0;
+        for (int i = 0; i < equation.length(); ++i) {
+            char c = equation.charAt(i);
+            if (!Pattern.matches("[A-Z]", Character.toString(c))) {
+                if (isOperator(c)) {
+                    indexZero++;
+                }
+            } else {
+                return false;
+            }
+        }
+        return indexZero != 0;
+    }
+
+    private boolean isOperator(char c){
+        return c =='+' || c == '-' || c =='/' || c == '*';
+    }
+
+}
---
+++ src/main/java/com/efimchik/ifmo/web/mvc/controllers/ValueController.java
@@ -0,0 +1,47 @@
+package com.efimchik.ifmo.web.mvc.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.regex.Pattern;
+
+@RestController
+public class ValueController {
+
+    @PutMapping("/calc/{path}")
+    public ResponseEntity<String> doPut(HttpSession session, @PathVariable String path, @RequestBody String body) {
+        if (goodFormat(body)) {
+            Object newPath = session.getAttribute(path);
+            session.setAttribute(path, body);
+            if (newPath != null) {
+                return new ResponseEntity<>(HttpStatus.valueOf(200));
+            }
+            else {
+                return new ResponseEntity<>(HttpStatus.valueOf(201));
+            }
+        } else {
+            return new ResponseEntity<>(HttpStatus.valueOf(403));
+        }
+    }
+
+    @DeleteMapping("/calc/{var}")
+    public ResponseEntity deleteVar(HttpSession session, @PathVariable String var) {
+        session.removeAttribute(var);
+        return new ResponseEntity(HttpStatus.valueOf(204));
+    }
+
+    private boolean goodFormat(String variable) {
+        char c = variable.charAt(0);
+        if (Pattern.matches("[a-z]", Character.toString(c)))
+            return true;
+        try {
+            return Integer.parseInt(variable) >= -10000 && Integer.parseInt(variable) <= 10000;
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+}
