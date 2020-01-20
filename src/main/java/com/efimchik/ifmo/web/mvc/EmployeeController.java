edda0ccf6cd3d9e48213c3a83dedccbccba63e59
KostMil <55847173+KostMil@users.noreply.github.com>
2020-01-20 04:23:59 +0300

    First try

---
+++ src/main/java/com/efimchik/ifmo/web/mvc/EmployeeController.java
@@ -0,0 +1,103 @@
+package com.efimchik.ifmo.web.mvc;
+
+import com.efimchik.ifmo.web.mvc.domain.Employee;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping("/employees")
+    public ResponseEntity<List<Employee>> getAll(@RequestParam(required = false) Integer page,
+                                                 @RequestParam(required = false) Integer size,
+                                                 @RequestParam(required = false) String sort) throws SQLException {
+        String from = buildFrom("SELECT * FROM EMPLOYEE", sort, page, size, null);
+        List<Employee> entities = ServiceFactoryController.allEmployees(from,false, true);
+        assert entities != null;
+        return ResponseEntity.ok(entities);
+    }
+
+    @GetMapping("/employees/{employee_id}")
+    public ResponseEntity<Employee> getById(@PathVariable(name = "employee_id") String employeeId,
+                                            @RequestParam(name = "full_chain", required = false, defaultValue = "false") String fullChain) throws SQLException {
+        String from = "SELECT * FROM EMPLOYEE WHERE id = " + employeeId;
+        if ("true".equals(fullChain)) {
+            return ResponseEntity.ok(Objects.requireNonNull(ServiceFactoryController.allEmployees(from, true, true)).get(0));
+        } else {
+            return ResponseEntity.ok(Objects.requireNonNull(ServiceFactoryController.allEmployees(from, false, true)).get(0));
+        }
+    }
+
+    @GetMapping("/employees/by_manager/{managerId}")
+    public ResponseEntity<List<Employee>> getByManagerId(@PathVariable(name = "managerId") Integer managerId,
+                                                         @RequestParam(required = false) Integer page,
+                                                         @RequestParam(required = false) Integer size,
+                                                         @RequestParam(required = false) String sort) throws SQLException {
+        String from = buildFrom("SELECT * FROM EMPLOYEE WHERE manager = ", sort, page, size, managerId);
+        List<Employee> entities = ServiceFactoryController.allEmployees(from, false, true);
+        assert entities != null;
+        return ResponseEntity.ok(entities);
+    }
+
+    @GetMapping("/employees/by_department/{depIdOrDepName}")
+    public ResponseEntity<List<Employee>> getByDepId(@PathVariable(name = "depIdOrDepName") String depIdOrDepName,
+                                                     @RequestParam(required = false) Integer page,
+                                                     @RequestParam(required = false) Integer size,
+                                                     @RequestParam(required = false) String sort) throws SQLException {
+        BigInteger depId;
+        if(!isNumber(depIdOrDepName)) {
+            String query = "SELECT id FROM DEPARTMENT WHERE name = '" + depIdOrDepName + "'";
+            depId = ServiceFactoryController.departmentByFrom(query);
+        } else {
+            depId = BigInteger.valueOf(Integer.parseInt(depIdOrDepName));
+        }
+        String fixedSort = changeName(sort);
+        String from = "SELECT * FROM EMPLOYEE WHERE department = " +
+                depId +
+                ((sort != null) ? " ORDER BY " + fixedSort : " ") +
+                ((size != null) ? " LIMIT " + size : " ") +
+                ((page != null) ? " OFFSET " + size * page : " ");
+//        String sql = buildFrom("SELECT * FROM EMPLOYEE WHERE department = ", sort, page, size, depId);
+        List<Employee> entities = ServiceFactoryController.allEmployees(from, false, true);
+        assert entities != null;
+        return ResponseEntity.ok(entities);
+
+    }
+
+    private static boolean isNumber(String smth){
+        try {
+            Integer.parseInt(smth);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static String changeName(String sort) {
+        if ("hired".equals(sort))
+            return "hiredate";
+        return sort;
+    }
+
+    private static String buildFrom(String from, String sort, Integer page, Integer size, Integer ID){
+        String newSort = changeName(sort);
+        String s1 = (page != null) ? " OFFSET " + size * page : " ";
+        if (ID == null) {
+            return from +
+                    ((sort != null) ? " ORDER BY " + newSort : " ") +
+                    ((size != null) ? " LIMIT " + size : " ") +
+                    s1;
+        } else {
+            return from + ID +
+                    ((sort != null) ? " ORDER BY " + newSort : " ") +
+                    ((size != null) ? " LIMIT " + size : " ") +
+                    s1;
+        }
+    }
+}
---
+++ src/main/java/com/efimchik/ifmo/web/mvc/ServiceFactoryController.java
@@ -0,0 +1,163 @@
+package com.efimchik.ifmo.web.mvc;
+
+import com.efimchik.ifmo.web.mvc.domain.Department;
+import com.efimchik.ifmo.web.mvc.domain.Employee;
+import com.efimchik.ifmo.web.mvc.domain.FullName;
+import com.efimchik.ifmo.web.mvc.domain.Position;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ServiceFactoryController {
+
+    private static ResultSet resSet(String s) {
+        try {
+            return DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "")
+                    .createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE).executeQuery(s);
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+
+    private static ResultSet resSetWithoutStatement(String s) {
+        try {
+            return DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "")
+                    .createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE).executeQuery(s);
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+
+    static List<Employee> allEmployees(String from, boolean bool, boolean isManager) {
+        ResultSet resultSet = resSet(from);
+        try {
+            if (resultSet != null) {
+                List<Employee> employees = new ArrayList<>();
+                while (resultSet.next()) {
+                    employees.add(employeeMapRow(resultSet,  bool, isManager));
+                }
+                return employees;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static Employee employeeMapRow(ResultSet resultSet, boolean bool, boolean isManager) throws SQLException {
+        Long id = Long. parseLong(resultSet.getString("id"));
+        FullName fullName = new FullName(
+                resultSet.getString("firstName"),
+                resultSet.getString("lastName"),
+                resultSet.getString("middleName")
+        );
+        List<Department> departments = departmentsList("select * from department");
+        Department department = null;
+        if (resultSet.getObject("department") != null) {
+            Long departmentId = Long.parseLong(resultSet.getString("department"));
+            department = departmentFound(departments, departmentId);
+        }
+        return new Employee(id,
+                fullName,
+                Position.valueOf(resultSet.getString("position")),
+                LocalDate.parse(resultSet.getString("hireDate")),
+                resultSet.getBigDecimal("salary"),
+                managerFound(resultSet, bool, isManager),
+                department);
+    }
+
+    private static Employee managerFound(ResultSet resultSet, boolean bool, boolean isManager) throws SQLException {
+        Employee manager = null;
+        if (resultSet.getObject("manager") != null && (bool || isManager)) {
+            BigInteger managerId = new BigInteger(resultSet.getString("manager"));
+            if (bool) {
+                manager = Objects.requireNonNull(allEmployees("SELECT * FROM employee WHERE id=" + managerId, true, false)).get(0);
+            } else {
+                manager = Objects.requireNonNull(allEmployees("SELECT * FROM employee WHERE id=" + managerId, false, false)).get(0);
+            }
+        }
+        return manager;
+    }
+
+//    private static Employee managerFound(BigInteger managerID, boolean bool, boolean isManager) {
+//        Employee manager = null;
+//        try {
+//            if ( managerID != null && isManager ) {
+//                boolean check = false;
+//                ResultSet ResultSet = resSet("select * from employee");
+//                assert ResultSet != null;
+//                while (ResultSet.next()) {
+//                    if (BigInteger.valueOf(ResultSet.getInt("id")).equals(managerID)) {
+//                        manager = allEmployees("select * from employee", bool, bool).get(0);
+//                        check = true;
+//                    }
+//                }
+//                if (!check) {
+//                    return null;
+//                }
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//        return manager;
+//    }
+
+    private static Department departmentMapRow(ResultSet resultSet) {
+        try {
+            Long ID = Long.parseLong(resultSet.getString("id"));
+            return new Department(ID,
+                    resultSet.getString("name"),
+                    resultSet.getString("location"));
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+
+    static BigInteger departmentByFrom(String from) throws SQLException {
+        ResultSet resultSet = resSetWithoutStatement(from);
+        try {
+            assert resultSet != null;
+            if (resultSet.next()) {
+                return new BigInteger(resultSet.getString("id"));
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static List<Department> departmentsList(String from) {
+        ResultSet resultSet = resSet(from);
+        List<Department> departments = new ArrayList<>();
+        try {
+            assert resultSet != null;
+            while (resultSet.next()) {
+                departments.add(departmentMapRow(resultSet));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return departments;
+    }
+
+    private static Department departmentFound(List<Department> departments, Long departmentID) {
+        Department departmentF = null;
+        for (Department department : departments) {
+            if (department.getId().equals(departmentID)) {
+                departmentF = department;
+            }
+        }
+        return departmentF;
+    }
+
+}
