b954a111d4bd593cdeb6c1387128008cb217d95d
Anastasia717 <asburtseva2000@gmail.com>
2020-02-15 14:50:40 +0300

    First try

---
+++ src/main/java/com/efimchik/ifmo/web/mvc/Equation.java
@@ -0,0 +1,76 @@
+package com.efimchik.ifmo.web.mvc;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import javax.servlet.http.HttpSession;
+import java.util.*;
+
+public class Equation {
+    public static ResponseEntity<String> calculateResult(HttpSession session) {
+        String equation = String.valueOf(session.getAttribute("equation"));
+        equation = equation.replaceAll("\\s+", "");
+        boolean error = false;
+        int i = 0;
+
+        while (i < equation.length()) {
+            if (equation.charAt(i) >= 'a' && equation.charAt(i) <= 'z') {
+                String value = String.valueOf(session.getAttribute(String.valueOf(equation.charAt(i))));
+                while (value.charAt(0) >= 'a' && value.charAt(0) <= 'z') {
+                    if (session.getAttribute(String.valueOf(equation.charAt(i))) == null) {
+                        error = true;
+                        break;
+                    }
+                    value = String.valueOf(session.getAttribute(value));
+                }
+                if (error)
+                    break;
+                equation = equation.replace(String.valueOf(equation.charAt(i)), value);
+            }
+            i++;
+        }
+
+        i = 1;
+        while (i < equation.length()) {
+            if (equation.charAt(i) == '-' && equation.charAt(i - 1) >= '0' && equation.charAt(i - 1) <= '9') {
+                equation = equation.replace(String.valueOf(equation.charAt(i)), "=");
+            }
+            i++;
+        }
+
+        String result;
+        if (error) {
+            int status = 409;
+            return new ResponseEntity<>(HttpStatus.valueOf(status));
+        } else {
+            List<String> expression = EquationUtils.parse(equation);
+            result = calc(expression);
+        }
+
+        int status = 200;
+        return new ResponseEntity<>(result, HttpStatus.valueOf(status));
+    }
+
+    private static String calc(List<String> postfix) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String x : postfix) {
+            if ("+".equals(x)) {
+                stack.push(stack.pop() + stack.pop());
+            } else if ("=".equals(x)) {
+                Integer b = stack.pop();
+                Integer a = stack.pop();
+                stack.push(a - b);
+            } else if ("*".equals(x)) {
+                stack.push(stack.pop() * stack.pop());
+            } else if ("/".equals(x)) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a / b);
+            } else {
+                stack.push(Integer.valueOf(x));
+            }
+        }
+        return String.valueOf(stack.pop());
+    }
+}
