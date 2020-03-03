7cb3d6043d19160fe79a7ac1762ecf3725be8781
Vgolovanov2000 <supersherman32@gmail.com>
2020-03-03 19:13:08 +0300

    fixed annotation import

--- src/main/java/com/efimchik/ifmo/web/mvc/CalcController.java
+++ src/main/java/com/efimchik/ifmo/web/mvc/CalcController.java
@@ -4,7 +4,10 @@ package com.efimchik.ifmo.web.mvc;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
 import java.util.*;
