4df5cdc550d9b7f0f63a1856a43f9b2b64fe63a4
Kemoler <s-s1990@yandex.ru>
2020-03-16 22:21:50 +0300

    task-2 mvc

---
+++ src/main/java/com/efimchik/ifmo/web/mvc/DataBase.java
@@ -0,0 +1,35 @@
+package com.efimchik.ifmo.web.mvc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DataBase {
+
+    // JDBC database URL
+    private static final String DB_URL = "jdbc:h2:mem:testdb";
+
+    //  Database credentials
+    private static final String USER = "sa";
+    private static final String PASS = "";
+
+    private static final DataBase instance = new DataBase();
+
+    public static DataBase instance() {
+        return instance;
+    }
+
+    public Connection createConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, USER, PASS);
+    }
+
+    private DataBase() {
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+
+    }
+}
