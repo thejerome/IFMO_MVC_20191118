11758ea0de729538616bcf4369f40db640d2c2c8
asvarish <42956531+asvarish@users.noreply.github.com>
2020-03-21 22:16:36 +0300

    Done

---
+++ src/main/java/com/efimchik/ifmo/web/mvc/Equation.java
@@ -0,0 +1,48 @@
+package com.efimchik.ifmo.web.mvc;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/calc/equation")
+public class Equation {
+    @PutMapping()
+    public ResponseEntity<String> put(@RequestBody String eq, HttpSession ses) {
+        if (!ok(eq)) return new ResponseEntity<>("bad", HttpStatus.BAD_REQUEST);
+        if (ok(eq)) {
+            if (ses.getAttribute("eq") == null) {
+                ses.setAttribute("eq", eq);
+                return new ResponseEntity<>(HttpStatus.CREATED);
+            }
+            else {
+                ses.setAttribute("eq", eq);
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+        }
+        return null;
+    }
+
+    @DeleteMapping()
+    public ResponseEntity del(HttpSession ses) {
+        ses.removeAttribute("eq");
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    private boolean ok(String eq) {
+        return checkEquation(eq);
+    }
+
+    private boolean checkEquation(String eq) {
+        boolean op = false;
+        for (char c : eq.toCharArray()) {
+            if (c >= 'A' && c <= 'Z')
+                return false;
+            if (c == '+' || c == '-' || c == '/' || c == '*')
+                op = true;
+        }
+        return op;
+    }
+}
