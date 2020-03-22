607fd305fe9df5e6a01043ae509b1ed500896af8
asvarish <42956531+asvarish@users.noreply.github.com>
2020-03-22 19:26:00 +0300

    commit 3

--- src/main/java/com/efimchik/ifmo/web/mvc/Vars.java
+++ src/main/java/com/efimchik/ifmo/web/mvc/Vars.java
@@ -2,6 +2,7 @@ package com.efimchik.ifmo.web.mvc;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 import javax.servlet.http.HttpSession;
