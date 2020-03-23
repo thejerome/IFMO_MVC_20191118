6976d1ddffa5054870d3cb916be21803a84d95f4
asvarish <42956531+asvarish@users.noreply.github.com>
2020-03-23 22:06:59 +0300

    commit 11111111111

--- src/main/java/com/efimchik/ifmo/web/mvc/Equation.java
+++ src/main/java/com/efimchik/ifmo/web/mvc/Equation.java
@@ -2,9 +2,11 @@ package com.efimchik.ifmo.web.mvc;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import javax.servlet.http.HttpSession;
 
 @RestController
--- src/main/java/com/efimchik/ifmo/web/mvc/Vars.java
+++ src/main/java/com/efimchik/ifmo/web/mvc/Vars.java
@@ -2,7 +2,11 @@ package com.efimchik.ifmo.web.mvc;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 
 import javax.servlet.http.HttpSession;
